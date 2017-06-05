90df33f8d35a404e6d85682e7658525839190e62
vrxfile <vrxfile@mail.ru>
2017-06-05 14:23:33 +0300

    Added ADC ADS1115 test

---
+++ meteo_ads1115/meteo_ads1115.ino
@@ -0,0 +1,36 @@
+/*
+  Пример работы с интегрированным АЦП ADS1115
+  Created by Rostislav Varzar, MGBot
+*/
+
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1115 ads;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  delay(1000);
+
+  // Инициализация АЦП
+  ads.setGain(GAIN_TWOTHIRDS);
+  ads.begin();
+  delay(1000);
+}
+
+void loop() {
+  // Измерение
+  float adc0 = (float)ads.readADC_SingleEnded(0) / 32767 * 6.144;
+  float adc1 = (float)ads.readADC_SingleEnded(1) / 32767 * 6.144;
+  float adc2 = (float)ads.readADC_SingleEnded(2) / 32767 * 6.144;
+  float adc3 = (float)ads.readADC_SingleEnded(3) / 32767 * 6.144;
+  // Вывод измеренных значений в терминал
+  Serial.println("ADC0: " + String(adc0, 3) + " V");
+  Serial.println("ADC1: " + String(adc1, 3) + " V");
+  Serial.println("ADC2: " + String(adc2, 3) + " V");
+  Serial.println("ADC3: " + String(adc3, 3) + " V");
+  Serial.println();
+  delay(1000);
+}
+
