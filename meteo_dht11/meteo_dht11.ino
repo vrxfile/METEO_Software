b3a188e26a17659ab97cdccaeea07e0895ca117f
vrxfile <vrxfile@mail.ru>
2017-06-05 12:44:57 +0300

    Added DHT11 and DS18B20 tests

---
+++ meteo_dht11/meteo_dht11.ino
@@ -0,0 +1,31 @@
+/*
+  Пример работы с внешним датчиком температуры/влажности DHT11
+  Created by Rostislav Varzar, MGBot
+*/
+
+#include <DHT.h>
+
+#define DHTPIN 14
+DHT dht(DHTPIN, DHT11, 15);
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  delay(1000);
+
+  // Инициализация датчика
+  dht.begin();
+  delay(1000);
+}
+
+void loop() {
+  // Измерение
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // Вывод измеренных значений в терминал
+  Serial.println("Temperature: " + String(t, 1) + " *C");
+  Serial.println("Humidity: " + String(h, 1) + " %");
+  Serial.println();
+  delay(1000);
+}
+
---
+++ meteo_ds18b20/meteo_ds18b20.ino
@@ -0,0 +1,32 @@
+/*
+  Пример работы с внешним датчиком температуры DS18B20
+  Created by Rostislav Varzar, MGBot
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 13
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature ds18b20(&oneWire);
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  delay(1000);
+
+  // Инициализация датчика
+  ds18b20.begin();
+  delay(1000);
+}
+
+void loop() {
+  // Измерение
+  ds18b20.requestTemperatures();
+  float t = ds18b20.getTempCByIndex(0);
+  // Вывод измеренных значений в терминал
+  Serial.println("Temperature: " + String(t, 1) + " *C");
+  Serial.println();
+  delay(1000);
+}
+
